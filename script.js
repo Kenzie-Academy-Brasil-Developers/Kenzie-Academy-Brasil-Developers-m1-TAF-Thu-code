2e940b9d56bc8dccd46f747ae816a5ad13724964
Thuani Dos Santos Nunes <74463786+thu-code@users.noreply.github.com>
2023-04-16 23:20:44 -0300

    código feito

--- script.js
+++ script.js
@@ -24,7 +24,51 @@ passed = false;
 // **sua lógica a partir daqui**
 
 
+if(gender === "male"){  //VERIFICA O GENERO 
+    if(height >= 1.70){ //VERIFICA A ALTURA 
+    
+        if(barReps >= 6 || barSeconds >= 15){  //VERIFICA REPETIÇÃO NA BARRA
 
+            if(abs >= 41){  //VERIFICA ABDOMINAIS
+
+                if(runDistance >= 3000 && runTime <= 720 ||  //VERIFICA DISTANCIA CORRIDA
+                    runDistance >= 5000 && runTime <= 1200){
+
+                    if(swimDistance >= 100 && swimTime <= 60 || diveTime <= 30){ //VERIFICA O NADO 
+                        passed = true;  
+                    }
+                }
+            }
+        }
+    }
+}
+    else if (gender === "female"){ // VERIFICA O GENERO 
+        if(height >= 1.60){ //VERIFICA A ALTURA
+
+            if(barReps >= 5 || barSeconds >= 12){ // VERIFICA BARRA
+
+                if(abs >= 41){ //VERIFICA ABDOMINAIS
+
+                    if(runDistance >= 4000 && runTime <= 900 || 
+                    runDistance >= 6000 && runTime <= 1320){ //VERIFICA CORRIDA 
+
+                        if(swimDistance >= 100 && swimTime <= 60 || 
+                        diveTime <= 30){ //VERIFICA NATAÇÃO
+                        passed = true;
+                        }
+                    } 
+                }
+            }
+        }
+    }else{
+    passed = false; //
+    }
+
+
+
+//
+    
+  
 /*
     Seu código deve conter apenas UM console.log, e ele deve ser o abaixo.
     Não altere nada pra baixo dessa linha, senão os testes não irão funcionar.
